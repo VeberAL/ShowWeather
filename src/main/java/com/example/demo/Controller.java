e8063e0994ce12837eba8944cda3a474708e2939
VeberAL <aleo83@rambler.ru>
2021-09-08 21:53:29 +0300

    Add FX Form

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,14 +1,29 @@
 package com.example.demo;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("ShowWeather!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button GETDATA;
+
+    @FXML
+    private TextField CITY;
+
+    @FXML
+    void initialize() {
+        assert GETDATA != null : "fx:id=\"GETDATA\" was not injected: check your FXML file 'Untitled'.";
+        assert CITY != null : "fx:id=\"CITY\" was not injected: check your FXML file 'Untitled'.";
+
     }
 }
